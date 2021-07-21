4d06968e7e19e7475138bc194ea3998d586080d1
Alastair Wilkes <awilkes@launchableinc.com>
2021-07-21 13:23:37 -0600

    Test Launchable post-merge data collection

--- post-merge.sh
+++ post-merge.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -ex
 
+BUILD_NAME=${GITHUB_RUN_ID:=local-$(date +%s)}
+
 pip3 install --user launchable~=1.0
+
 launchable verify
 
+launchable record build --name "$BUILD_NAME" --source src=.
+
 ./gradlew test
+
+function record() {
+  launchable record tests --build "$BUILD_NAME" gradle build/test-results/test
+}
+
+trap record EXIT
